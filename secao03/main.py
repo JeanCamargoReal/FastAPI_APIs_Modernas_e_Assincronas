484837240a766c8b787d42799ff32e8432e2c1f8
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-02-08 10:27:12 -0300

    Injeção de dependências

--- secao03/main.py
+++ secao03/main.py
@@ -1,5 +1,18 @@
-from fastapi import FastAPI, HTTPException, Path, Response, status
+from time import sleep
+from typing import Any
+
+from fastapi import Depends, FastAPI, HTTPException, Path, Response, status
 from models import Curso
+
+
+def fake_db():
+    try:
+        print("Abrindo conexão com o banco de dados...")
+        sleep(1)
+    finally:
+        print("Fechando conexão com o banco de dados...")
+        sleep(1)
+
 
 app = FastAPI()
 
@@ -10,7 +23,7 @@ cursos = {
 
 
 @app.get("/cursos")
-async def get_cursos():
+async def get_cursos(db: Any = Depends(fake_db)):
     return cursos
 
 
@@ -24,7 +37,8 @@ async def get_curso(
         gt=0,
         # lt = lower then
         lt=3,
-    )
+    ),
+    db: Any = Depends(fake_db),
 ):
     try:
         curso = cursos[curso_id]
@@ -38,7 +52,7 @@ async def get_curso(
 
 
 @app.post("/cursos", status_code=status.HTTP_201_CREATED)
-async def post_curso(curso: Curso):
+async def post_curso(curso: Curso, db: Any = Depends(fake_db)):
     next_id: int = len(cursos) + 1
     cursos[next_id] = dict(curso)
     cursos[next_id]["id"] = next_id
@@ -47,7 +61,7 @@ async def post_curso(curso: Curso):
 
 
 @app.put("/cursos/{curso_id}")
-async def put_curso(curso_id: int, curso: Curso):
+async def put_curso(curso_id: int, curso: Curso, db: Any = Depends(fake_db)):
     if curso_id in cursos:
         cursos[curso_id] = dict(curso)
         curso.id = curso_id
@@ -63,7 +77,7 @@ async def put_curso(curso_id: int, curso: Curso):
 
 
 @app.delete("/cursos/{curso_id}")
-async def delete_curso(curso_id: int):
+async def delete_curso(curso_id: int, db: Any = Depends(fake_db)):
     if curso_id in cursos:
         del cursos[curso_id]
         return Response(status_code=status.HTTP_204_NO_CONTENT)
