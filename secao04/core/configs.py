8466807d50d42854cd03baeba79ee10e7fc654ae
JeanCamargoReal <jeancamargoreal@icloud.com>
2023-02-11 18:40:01 -0300

    Cria tabelas

--- secao04/core/configs.py
+++ secao04/core/configs.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     # Configurações gerais usadas na aplicação
 
     API_V1_STR: str = "/api/v1"
-    DB_URL: str = "postgresql+asyncpg://postgres@localhost:5432/FastAPI"
+    DB_URL: str = "postgresql+asyncpg://postgres@localhost:5432/fastapi"
     DBBaseModel = declarative_base()
 
     class Config:
